8961f0e7000a96a9807c4e98b065abcc06abdd6b
JHOproject <yabe55230@gmail.com>
2021-04-30 19:38:29 +0800

    引入 vuex & sass

---
+++ src/store/index.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex) // 告訴 Vue 使用 vuex
+
+// 定義一個新的 Vue Store
+/* eslint-disable */ 
+const store = new Vuex.Store({
+  state: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  getters: {
+  }
+})
+export default store
